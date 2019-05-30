3a4f73ba1e0275c9ed0047128ade1ec9d5cfabfe
Jens Opsvik <jensemann92@gmail.com>
2019-05-30 16:27:43 +0200

    fungera med fargevindu

--- index.js
+++ index.js
@@ -12,14 +12,32 @@ function drop(ev){
 }
 
 
+
+
 function addNewTask(){
     let new_task = document.createElement("div");
     new_task.classList.add("task");
     new_task.setAttribute("draggable", "true");
     new_task.setAttribute("ondragstart", "dragStart(event)");
+    
+    window.onerror = function(message){
+        window.confirm("You have to create a table before you add a task.");
+    }
 
-    document.getElementById("table").appendChild(new_task);
-    new_task.id = "cc";
-    console.log(new_task);
+    document.getElementById("addedTable").appendChild(new_task);
+    
+    let color = prompt("green, blue, cyan, red")
+    new_task.id = color;
 
+}
+
+function addNewTable(){
+    let new_table = document.createElement("div");
+    new_table.classList.add("newTable");
+    new_table.setAttribute("ondragover", "allowDrop(event)");
+    new_table.setAttribute("ondrop", "drop(event)");
+    document.getElementById("tableContainer").appendChild(new_table);
+    new_table.id = "addedTable"
+    console.log(new_table);
+    
 }
