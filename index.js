0c8fa3589b790ea8e5d837bb0012926bbe154658
Jeppe <jensemann92@gmail.com>
2019-05-31 16:05:05 +0200

    fungerer med dropdown

--- index.js
+++ index.js
@@ -14,26 +14,6 @@ function drop(ev){
 }
 
 
-function addNewTable(){
-    let tekstheader = prompt("Navn på table");
-    let new_table = document.createElement("div");
-    new_table.classList.add("newTable");
-    new_table.setAttribute("ondragover", "allowDrop(event)");
-    new_table.setAttribute("ondrop", "drop(event)");
-    document.getElementById("tableContainer").appendChild(new_table);
-    new_table.id = tekstheader;
-    console.log(new_table);
-    
-    let table_header = document.createElement("h2");
-    document.getElementById(tekstheader).appendChild(table_header);
-    
-  
-    let text = document.createTextNode(tekstheader)
-    table_header.appendChild(text);
-    document.getElementById(tekstheader).appendChild(table_header);
-    
-    
-}
 
 function addNewTask(){
     let new_task = document.createElement("div");
@@ -54,3 +34,49 @@ function addNewTask(){
 
 }
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function addTaskTodo(){
+    let todoTask = document.createElement("div");
+    todoTask.classList.add("task");
+    todoTask.setAttribute("draggable", "true");
+    todoTask.setAttribute("ondragstart", "dragStart(event)");
+    document.getElementById("1").appendChild(todoTask);
+    todoTask.id = "c"
+}
+
+function addTaskDoing(){
+    let doingTask = document.createElement("div");
+    doingTask.classList.add("task");
+    doingTask.setAttribute("draggable", "true");
+    doingTask.setAttribute("ondragstart", "dragStart(event)");
+    document.getElementById("2").appendChild(doingTask);
+    doingTask.id = "e"
+}
+
+function addTaskDone(){
+    let doneTask = document.createElement("div");
+    doneTask.classList.add("task");
+    doneTask.setAttribute("draggable", "true");
+    doneTask.setAttribute("ondragstart", "dragStart(event)");
+    document.getElementById("3").appendChild(doneTask);
+    doneTask.id = "d"
+}
